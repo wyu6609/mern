3155d080391b454fb9373b7184c0c927fb84313e
wyu6609 <wy427.nyu.edu>
2022-08-03 10:27:04 -0400

    first commit

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+//@desc get goals
+//@route GET /api/goals
+//@access Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+};
+
+//@desc Set goals
+//@route POST /api/goals
+//@access Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set goal" });
+};
+
+//@desc Update goal
+//@route PUT /api/goals
+//@access Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+//@desc delete goals
+//@route DELETE /api/goals
+//@access Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
