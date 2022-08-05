270950b12ddcc0a1e940153d06824fdf38d071ca
wyu6609 <wy427.nyu.edu>
2022-08-05 11:25:29 -0400

    added user login and create routes

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //routes
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 //error handling
 app.use(errorHandler);
 
