704d3d6a5d8945311de0059f54131fc75ee5e10c
wyu6609 <wy427.nyu.edu>
2022-08-05 10:39:52 -0400

    config added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,14 @@
 const asyncHandler = require("express-async-handler");
+
+const Goal = require("../models/goalModel");
+
 //@desc get goals
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get goals" });
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
 
 //@desc Set goals
@@ -14,22 +19,45 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
+  const goals = await Goal.create({
+    text: req.body.text,
+  });
 
-  res.status(200).json({ message: "Set goal" });
+  res.status(200).json(goals);
 });
 
 //@desc Update goal
 //@route PUT /api/goals
 //@access Private
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 //@desc delete goals
 //@route DELETE /api/goals
 //@access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
---
+++ backend/models/goalModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const goalSchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "Please add a text value"],
+    },
+  },
+  {
+    timestamps: true, // updated_at, created_at
+  }
+);
+
+module.exports = mongoose.model("Goal", goalSchema);
